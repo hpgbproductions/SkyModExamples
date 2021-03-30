93da3780e8c12b236c7da9577f3198e382e932d8
hpgbproductions <36079650+hpgbproductions@users.noreply.github.com>
2021-03-30 16:51:40 +0800

    Add files via upload

---
+++ Assets/Examples.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+public class Examples : MonoBehaviour
+{
+    // Sky Dome GameObject.
+    private GameObject SkyDome;
+
+    // Sky Dome "TOD_Sky" component type
+    private Type SkyComponentType;
+
+    // A reference to the actual component of type "TOD_Sky"
+    private object SkyComponent;
+
+    // "TOD_AtmosphereParameters" type found in "TOD_Sky"
+    private Type AtmosphereType;
+
+    // A reference to the object of type "TOD_AtmosphereParameters" in SkyComponent
+    private object Atmosphere;
+
+    // The brightness field defined in the "TOD_AtmosphereParameters" type
+    private FieldInfo BrightnessField;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        ServiceProvider.Instance.DevConsole.RegisterCommand<float>("ChangeSkyBrightness", ChangeSkyBrightness);
+    }
+
+    // This is called by our dev console command
+    private void ChangeSkyBrightness(float brightness)
+    {
+        // Get all components, and looks through them to find a TOD_Sky component
+        Component[] AllComponents = FindObjectsOfType<Component>();
+        foreach (Component c in AllComponents)
+        {
+            if (c.GetType().Name == "TOD_Sky")
+            {
+                // Saves Sky Dome information
+                SkyDome = c.gameObject;
+                SkyComponentType = c.GetType();
+                SkyComponent = c;
+            }
+        }
+        if (SkyDome == null)
+        {
+            // Returns if no TOD_Sky component is currently loaded
+            // Occurs in the designer, or if the Sky Dome was removed by another script
+            Debug.LogError("No SkyDome found");
+            return;
+        }
+
+        // Get all fields in the TOD_Sky type,
+        // and looks through them to find the Atmosphere field
+        FieldInfo[] fields = SkyComponentType.GetFields();
+        foreach (FieldInfo field in fields)
+        {
+            if (field.Name == "Atmosphere")    // also try (field.FieldType.Name == "TOD_AtmosphereParameters")
+            {
+                AtmosphereType = field.FieldType;
+                Atmosphere = field.GetValue(SkyComponent);
+
+                // Get all fields in the TOD_AtmosphereParameters type,
+                // and looks through them to find the Brightness field
+                FieldInfo[] atmoFields = AtmosphereType.GetFields();
+                foreach (FieldInfo atmoField in atmoFields)
+                {
+                    if (atmoField.Name == "Brightness")
+                        BrightnessField = atmoField;
+                }
+            }
+        }
+
+        float OldValue = (float)BrightnessField.GetValue(Atmosphere);
+        Debug.Log("Old brightness was " + OldValue);
+
+        // Sets the value of the Brightness field in the Atmosphere object
+        // to the one entered in the command
+        BrightnessField.SetValue(Atmosphere, brightness);
+        Debug.Log("New brightness is " + brightness);
+    }
+}
---
+++ Assets/Examples2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+public class Examples2 : MonoBehaviour
+{
+    [SerializeField] private Material SkyboxMaterial = null;
+
+    private Camera MainCamera;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GameObject SkyDome = null;
+
+        Type SkyComponentHolderType = null;
+        object SkyComponentHolder = null;
+
+        // Similar Component finder to the first example
+        Component[] AllComponents = FindObjectsOfType<Component>();
+        foreach (Component c in AllComponents)
+        {
+            if (c.GetType().Name == "TOD_Components")
+            {
+                SkyDome = c.gameObject;
+                SkyComponentHolderType = c.GetType();
+                SkyComponentHolder = c;
+            }
+        }
+        if (SkyDome == null)
+        {
+            Debug.LogError("No SkyDome found");
+            return;
+        }
+
+        // Similar Field finder to the first example
+        FieldInfo[] fields = SkyComponentHolderType.GetFields();
+        foreach (FieldInfo field in fields)
+        {
+            // Get all GameObjects referenced by TOD and destroy them
+            if (field.FieldType.Name == "GameObject")
+            {
+                GameObject RefGameObject = (GameObject)field.GetValue(SkyComponentHolder);
+                Debug.Log("Destroyed GameObject " + RefGameObject.name);
+                Destroy(RefGameObject);
+            }
+        }
+
+        // Finally destroy the Sky Dome itself to prevent errors from appearing in the dev console
+        Debug.Log("Destroyed GameObject " + SkyDome.name);
+        Destroy(SkyDome);
+
+        // Replace the skybox, if a skybox material is defined
+        if (SkyboxMaterial)
+        {
+            RenderSettings.skybox = SkyboxMaterial;
+        }
+
+        // Set up the MainCamera to use the skybox
+        // Since there is more than one Camera tagged MainCamera, we cannot use Camera.main
+        // and must instead find the correct camera by name
+        MainCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
+        MainCamera.clearFlags = CameraClearFlags.Skybox;
+        Debug.Log("Applied settings on Camera " + MainCamera.name);
+    }
+}
